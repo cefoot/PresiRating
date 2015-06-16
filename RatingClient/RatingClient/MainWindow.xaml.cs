c19fef0cd015196a426247ae61b573aeae67dacb
cefoot <cefoot@outlook.de>
2015-06-16 21:11:59 +0200

    first working version =)

---
+++ RatingClient/RatingClient/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WebSocketSharp;
+
+namespace RatingClient
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        WebSocket WebSocket { get; set; }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WebSocket = new WebSocket("ws://cefoot.de:8080/rating-server/rating");
+            WebSocket.OnMessage += ShowRating;
+
+            WebSocket.Connect();
+        }
+
+        private void ShowRating(object sender, MessageEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                var rand = new Random();
+                var rating = new RatingWindow(e.Data.Substring("rating:".Length));
+                rating.Left = rand.Next((int)SystemParameters.PrimaryScreenWidth);
+                rating.Top = rand.Next((int)SystemParameters.PrimaryScreenHeight);
+                rating.Show();
+            });
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            WebSocket.Close();
+        }
+
+        private void ButonStopClick(object sender, RoutedEventArgs e)
+        {
+            WebSocket.Close();
+        }
+
+        private void ButonStartClick(object sender, RoutedEventArgs e)
+        {
+            WebSocket.Connect();
+        }
+    }
+}
---
+++ RatingClient/RatingClient/RatingWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RatingClient
+{
+    /// <summary>
+    /// Interaktionslogik für RatingWindow.xaml
+    /// </summary>
+    public partial class RatingWindow : Window
+    {
+        public RatingWindow(String ratingImg)
+        {
+            InitializeComponent();
+            //var bitmap = Properties.Resources.ResourceManager.GetObject("rating_" + ratingImg, Properties.Resources.Culture);
+            img.Source = new BitmapImage(new Uri("pack://siteoforigin:,,,/Resources/rating_"+ratingImg+".png"));
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(Waiting);
+        }
+
+        private void Waiting(object state)
+        {
+            var start = DateTime.Now;
+            while ((DateTime.Now - start).TotalSeconds < 5)
+            {
+                Thread.Sleep(100);
+            }
+            Dispatcher.Invoke(() => this.Close());
+        }
+    }
+}
